7397b73c4e56c94cc3787b7a93df0461984a54a8
Iltotore <rafbodaha@gmail.com>
2021-08-29 15:39:26 +0200

    feat: Add ResolutionError aliases

--- core/src/io/github/fluent4s/error/ResolutionError.scala
+++ core/src/io/github/fluent4s/error/ResolutionError.scala
@@ -5,4 +5,8 @@ case class ResolutionError(message: String) extends Error
 object ResolutionError {
 
   def NotFound(value: String): ResolutionError = ResolutionError(s"Not found: $value")
+
+  def Mismatch(expected: String, got: String): ResolutionError = ResolutionError(s"Expected $expected, got $got")
+
+  val Impossible: ResolutionError = ResolutionError("Impossible error. This is probably a bug.")
 }
