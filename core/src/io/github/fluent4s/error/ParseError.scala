b7d0dfb8667acf7698c051a04ec1acd945193f89
Iltotore <rafbodaha@gmail.com>
2021-09-02 11:54:05 +0200

    feat: Propagate error message to parent

--- core/src/io/github/fluent4s/error/ParseError.scala
+++ core/src/io/github/fluent4s/error/ParseError.scala
@@ -1,5 +1,3 @@
 package io.github.fluent4s.error
 
-import io.github.fluent4s.syntax.Cursor
-
-class ParseError(message: String) extends Error
+class ParseError(message: String) extends Error(message)
