d2beff202578882ad6f429725122829472a8f865
Iltotore <rafbodaha@gmail.com>
2021-09-07 19:33:16 +0200

    feat: Make resolution context implicit

--- core/src/io/github/fluent4s/rst/ResolvedBase.scala
+++ core/src/io/github/fluent4s/rst/ResolvedBase.scala
@@ -19,13 +19,13 @@ trait ResolvedBase {
 
   implicit object ResourceResolver extends Resolver[FResource, RResource] {
 
-    override def resolve(input: FResource)(context: Context): Resolution[RResource] = {
+    override def resolve(input: FResource)(implicit context: Context): Resolution[RResource] = {
       input
         .body
         .filterType[ReferenceEntry]
         .foldLeft(context.validNel[ResolutionError])((ctxResolution: Resolution[Context], entry: ReferenceEntry) => (
             ctxResolution,
-            ctxResolution.andThen(entry.resolve)
+            ctxResolution.andThen(entry.resolve(_))
             ).mapN(_.withReference(entry.body.id.name, _))
         )
         .map(ctx => RResource(ctx.references))
@@ -34,20 +34,20 @@ trait ResolvedBase {
 
   implicit object AttributeResolver extends Resolver[FAttribute, RAttribute] {
 
-    override def resolve(input: FAttribute)(context: Context): Resolution[(String, RPattern)] =
-      input.value.resolve(context).map((input.id.name, _))
+    override def resolve(input: FAttribute)(implicit context: Context): Resolution[(String, RPattern)] =
+      input.value.resolve.map((input.id.name, _))
   }
 
   implicit object ExpressionResolver extends Resolver[FExpression, RExpression] {
 
-    override def resolve(input: FExpression)(context: Context): Resolution[RExpression] = input match {
+    override def resolve(input: FExpression)(implicit context: Context): Resolution[RExpression] = input match {
 
       case Select(selector, variants) => (
         selector.resolve(context),
-        variants.map(_.resolve(context)).sequence
+        variants.map(_.resolve).sequence
         ).mapN(RSelect.apply)
 
-      case Inline(body) => body.resolve(context).map(RInline.apply)
+      case Inline(body) => body.resolve.map(RInline.apply)
     }
   }
 }
